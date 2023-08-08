c621f59ff1657117b07ca1e6a67c7da6500e28aa
MagBachir <magagi.bachir1@gmail.com>
2023-08-08 15:28:18 +0100

    feat: Players services classe and players contoller generation without any function

---
+++ src/players/players.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { PlayersService } from './players.service';
+import { CreatePlayerDto } from './dto/create-player.dto';
+import { UpdatePlayerDto } from './dto/update-player.dto';
+
+@Controller('players')
+export class PlayersController {
+  constructor(private readonly playersService: PlayersService) {}
+
+}
---
+++ src/players/players.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+import { CreatePlayerDto } from './dto/create-player.dto';
+import { UpdatePlayerDto } from './dto/update-player.dto';
+
+@Injectable()
+export class PlayersService {
+  
+}
